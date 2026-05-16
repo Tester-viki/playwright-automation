f2ad0b08b19a338405799af09f25fca6a2f89f3a
Tester-viki <vickyrajaidp@gmail.com>
2026-05-16 22:27:10 +0530

    Updated Playwright configuration and test script

--- Locator.spec.js
+++ Locator.spec.js
@@ -1,32 +1,34 @@
-//const {test, expect} = require('@playwright/test')
-import {test, expect} from '@playwright/test';
-import { defineConfig, devices } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
-export default defineConfig({
-  use: {
-    headless: false,   // 👈 this makes browser visible
-    browserName: 'chromium', // Chrome engine
-    channel: 'chrome', // 👈 real Chrome browser
-  },
-});
-test ('Locators', async({page})=>{
+test('Locators', async ({ page }) => {
 
-   await page.goto("https://demoblaze.com/")
+  await page.goto("https://demoblaze.com/");
 
-   await page.waitForTimeout(3000)
-   // click on login button
-  // await page.locator('id=#login2').click()
-    
-  await page.click('#login2')
+  await page.waitForTimeout(3000);
 
-  await page.waitForTimeout(2000)
-  await page.fill ("#loginusername","pavanol")
+  // click login
+  await page.click('#login2');
+
+  await page.waitForTimeout(2000);
+
+  // username
+  await page.fill("#loginusername", "pavanol");
+
+  await page.waitForTimeout(2000);
+
+  // password
+  await page.fill("#loginpassword", "test@123");
+
+  // login button
+  await page.click("button[onclick='logIn()']");
+
+  await page.waitForTimeout(3000);
+
+  // verify logout visible
+  const logoutlink = page.locator("#logout2");
+
+  await expect(logoutlink).toBeVisible();
+
+  await page.close();
   
-  await page.waitForTimeout(2000)
-  await page.fill ("//input[@id='loginpassword']","test@123")
-
-  await page.click("button[onclick='logIn()']")
- 
-  await page.waitForTimeout(2000)
-  await page.close;
-})
+});
