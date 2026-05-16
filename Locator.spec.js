20d45a93a012f6ac44d360df3b79fe793302fcc7
Tester-viki <vickyrajaidp@gmail.com>
2026-05-16 12:18:15 +0530

    Initial commit

--- Locator.spec.js
+++ Locator.spec.js
@@ -9,7 +9,6 @@ export default defineConfig({
     channel: 'chrome', // 👈 real Chrome browser
   },
 });
-
 test ('Locators', async({page})=>{
 
    await page.goto("https://demoblaze.com/")
@@ -17,8 +16,17 @@ test ('Locators', async({page})=>{
    await page.waitForTimeout(3000)
    // click on login button
   // await page.locator('id=#login2').click()
-     await page.click('#login2')
+    
+  await page.click('#login2')
 
-     await page.waitForTimeout(2000)
-     await page.close;
+  await page.waitForTimeout(2000)
+  await page.fill ("#loginusername","pavanol")
+  
+  await page.waitForTimeout(2000)
+  await page.fill ("//input[@id='loginpassword']","test@123")
+
+  await page.click("button[onclick='logIn()']")
+ 
+  await page.waitForTimeout(2000)
+  await page.close;
 })
